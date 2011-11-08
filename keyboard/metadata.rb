f82a0e5f40c7ecb7a39fbd7f22e4776bcd4b31d9
Ryutaro YOSHIBA <ryuzee@gmail.com>
2011-11-08 10:43:25 +0900

    Update metadata

--- keyboard/metadata.rb
+++ keyboard/metadata.rb
@@ -1,6 +1,6 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
+maintainer       "Ryuzee"
+maintainer_email "ryuzee@gmail.com"
+license          "MIT License"
 description      "Installs/Configures keyboard"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
