09baf1947dbc324b7f4fb6b8772f94d2fcc4b640
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-02-21 14:41:23 +0900

    delete old recipes

--- chef_timeout_patch/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Ryuzee"
-maintainer_email "ryuzee@gmail.com"
-license          "MIT License"
-description      "Installs/Configures chef_timeout_patch"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
