21bcaf85f4d1c44aa39ccc5d3512ebe06e95d7cb
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-02-21 16:10:11 +0900

    rename and update recipe

--- mysql_remi_epel/metadata.rb
+++ mysql_remi_epel/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Ryuzee"
 maintainer_email "ryuzee@gmail.com"
 license          "MIT License"
-description      "Installs/Configures mysql"
+description      "Installs/Configures mysql with remi and epel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
---
+++ mysql_remi_epel/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: mysql
+# Recipe:: default
+#
+# Copyright 2013, Ryuzee
+#
+# MIT License
+#
+
+case node[:platform]
+when "centos"
+  if node[:platform_version] >= "5.0" and node[:platform_version] <= "5.9"
+  then
+    include_recipe "rpmrepos"
+    # change yum settings
+    %w{/etc/yum.repos.d/remi.repo /etc/yum.repos.d/epel.repo}.each do |file|
+      e = execute "sed -e s/enabled=0/enabled=1/ ".concat(file).concat(" > /tmp/1; mv /tmp/1 ").concat(file) do
+        action :run
+      end
+    end
+
+    %w{mysql-server}.each do |package_name|
+      yum_package package_name do
+        action :install
+        # for perl-DBI, I did not disable centos standard repository
+        options "--enablerepo=remi"
+      end
+    end
+
+    service "mysqld" do
+      action :restart
+      supports :status => true, :start => true, :stop => true, :restart => true
+    end
+
+    # restore yum settings
+    %w{/etc/yum.repos.d/remi.repo /etc/yum.repos.d/epel.repo}.each do |file|
+      e = execute "sed -e s/enabled=1/enabled=0/ ".concat(file).concat(" > /tmp/1; mv /tmp/1 ").concat(file) do
+        action :run
+      end
+    end
+  end
+end
