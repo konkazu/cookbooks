936d880a33d629976d96cbb9c4ed96b74b757652
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-02-21 14:44:15 +0900

    delete old recipes

--- iptables_web/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Ryuzee"
-maintainer_email "ryuzee@gmail.com"
-license          "MIT License"
-description      "Installs/Configures iptables_web"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
