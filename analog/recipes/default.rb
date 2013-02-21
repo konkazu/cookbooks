19028cfc5b3b080fc41510cfbcf8b3a7ba462757
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-02-21 14:27:53 +0900

    update analog

--- analog/recipes/default.rb
+++ analog/recipes/default.rb
@@ -2,19 +2,27 @@
 # Cookbook Name:: analog
 # Recipe:: default
 #
-# Copyright 2012, ryuzee 
+# Copyright 2013, ryuzee 
 #
 # MIT License 
 #
 case node[:platform]
-when "centos"
-  # install epel
-  e = execute "wget -O /tmp/analog-6.0.4-1.el5.i386.rpm http://www.iddl.vt.edu/~jackie/analog/analog-6.0.4-1.el5.i386.rpm" do
+when "centos", "amazon"
+  f = "analog-6.0.4-1.x86_64.rpm"
+  if node[:platform] == "centos" then
+    if node[:platform_version] >= "6.0" then 
+      f = "analog-6.0.4-1.x86_64.rpm"
+    elsif node[:platform_version] >= "5.0" then 
+      f = "analog-6.0.4-1.el5.i386.rpm"
+    end
+  end
+  e = execute "wget -O /tmp/#{f} http://www.iddl.vt.edu/~jackie/analog/#{f}" do
     action :run
   end
+
   package "analog" do
     action :install
-    source "/tmp/analog-6.0.4-1.el5.i386.rpm"
+    source "/tmp/#{f}"
     provider Chef::Provider::Package::Rpm
   end
 end
