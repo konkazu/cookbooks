ba5ffbec05f6db9cfc920b486d4a744bbe9fb960
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-02-21 22:23:21 +0900

    fix foodcritic warnings of FC008

--- samba/metadata.rb
+++ samba/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "ryuzee"
+maintainer_email "ryuzee@gmail.com"
 license          "All rights reserved"
 description      "Installs/Configures samba"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
