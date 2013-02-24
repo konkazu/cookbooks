d660cb0c628d06841762cf3f0868f92b8040a0ed
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-02-24 11:21:30 +0900

    delete old recipe

--- php5.3/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-default["memory_limit"] = 128 
-default["max_execution_time"] = 10
-default["upload_max_filesize"] = 20
-
--- php5.3/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             "php5.3"
-maintainer       "Ryuzee"
-maintainer_email "ryuzee@gmail.com"
-license          "MIT License"
-description      "Installs/Configures php5.3"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
